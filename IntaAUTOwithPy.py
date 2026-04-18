a43c43b1fd95fadc132a426ceb695533968bbbb2
Nathan Almeida <almeidanathan78@gmail.com>
2026-04-17 22:24:26 -0300

    Introdução

--- IntaAUTOwithPy.py
+++ IntaAUTOwithPy.py
@@ -1,1 +1,5 @@
-print (Olá pessoal, espero ver só automações aqui além desse código escrito )
+print("Olá pessoal, espero ver só automações aqui além desse código escrito")
+print("Esse código é só para testar o git, não tem nada a ver com automação")
+print(
+    "Mas se quiserem, podem usar ele para testar o git também, só não esqueçam de apagar depois"
+)
